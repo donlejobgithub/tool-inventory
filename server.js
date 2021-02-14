f4b3e107b4438c6dd5f32f6d9aa6774896f3eb61
Don Le <donle@Dons-Mac-mini.home>
2021-02-13 18:22:46 -0600

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonserver = reqire('json-server');
+const server = jsonServer.create();
+const router =  jsonServer.router('tools.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3004;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
